babba1c73a39bb41eb95d71fa3c5f8261e7ad1e8
Crowdbotics <engineering@crowdbotics.com>
2023-05-25 12:13:18 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "aged-leaf-dev-86093",
+  url: "https://aged-leaf-dev-86093.botics.co",
+  api: "https://aged-leaf-dev-86093.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
